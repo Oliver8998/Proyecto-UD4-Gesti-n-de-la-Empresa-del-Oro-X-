f5b7cc5c51d75fcf631624a9de38fac9d7bf3945
Oliver8998 <162971483+Oliver8998@users.noreply.github.com>
2025-11-25 21:13:00 +0100

    Creacion de la clase factory mediante la cual creamos las tablas vacías en PostgreSQL a partir de los modelos

--- db/factory/factory.py
+++ db/factory/factory.py
@@ -1,1 +1,9 @@
-#factoría
+from db.models.models import Base
+from db.config import engine
+
+def crear_tablas():
+    Base.metadata.create_all(bind=engine)
+    print("tablas creadas correctamente")
+
+if __name__ == "__main__":
+    crear_tablas()
